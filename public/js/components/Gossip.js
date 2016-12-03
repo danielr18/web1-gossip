5d676bab2b591d030bdfbcea01dd6f4b0578b0b6
Graren <rennycol006@gmail.com>
2016-12-03 14:29:59 -0400

    Up and Down Promises

--- public/js/components/Gossip.js
+++ public/js/components/Gossip.js
@@ -36,8 +36,60 @@ class Gossip {
     });
   }
 
+  up() {
+    console.log("up");
+    return new Promise((resolve, reject) => {
+      const XHR = new XMLHttpRequest();
+      XHR.open('post', '/gossip/up', true);
+
+      XHR.onload = function(e) {
+        //TODO: Grab data from response and set it to the object
+        if (e.target.status === '200') {
+          resolve(e.target.response);
+        } else {
+          reject({
+            message: "Something went wrong"
+          });
+        }
+      };
+
+      XHR.setRequestHeader('Content-type', 'application/json');
+      const payload = {
+        id_usuario: this.id_user,
+        id_gossip: this.id_gossip
+      }
+      XHR.send(JSON.stringify(payload));
+    });
+  }
+
+  down() {
+    return new Promise((resolve, reject) => {
+      const XHR = new XMLHttpRequest();
+      XHR.open('post', '/gossip/down', true);
+
+      XHR.onload = function(e) {
+        //TODO: Grab data from response and set it to the object
+        if (e.target.status === '200') {
+          resolve(e.target.response);
+        } else {
+          reject({
+            message: "Something went wrong"
+          });
+        }
+      };
+
+      XHR.setRequestHeader('Content-type', 'application/json');
+      const payload = {
+        id_usuario: this.id_user,
+        id_gossip: this.id_gossip
+      }
+      XHR.send(JSON.stringify(payload));
+    });
+  }
+
   render() {
     const gossip = document.createElement('div');
+    const self = this;
     gossip.className = 'gossip notification';
     gossip.innerHTML = '<div class="gossip-wrapper"> <div class="gossip-karma-wrapper"> <div class="vote-wrapper"> <a class="vote-btn positive-vote"> <span class="icon is-small"> <i class="fa fa-arrow-up"></i> </span> </a> </div> <div class="karma-wrapper"> <span class="gossip-karma"></span> </div> <div class="vote-wrapper"> <a class="vote-btn negative-vote"> <span class="icon is-small"> <i class="fa fa-arrow-down"></i> </span> </a> </div> </div> <div class="gossip-content"> <div class="gossip-header"> <p> <strong class="gossip-user"></strong> <span>-</span> <small class="gossip-date"></small> </p> </div> <div class="gossip-body"> <p class="gossip-description"></p> </div> </div> </div>';
     if (localStorage.user) {
@@ -52,7 +104,35 @@ class Gossip {
     gossip.querySelector('.gossip-description').textContent = this.description;
     gossip.querySelector('.gossip-date').textContent = this.date.toUTCString();
     gossip.querySelector('.gossip-karma').textContent = this.karma;
+
+    gossip.querySelector('.negative-vote').addEventListener('mouseup', function() {
+      //No puedo usar this porq es un nuevo contexto verdad?
+      self.down()
+        .then(function(response) {
+          gossip.karma.textContent = response.data.ka_gossip;
+        })
+        .catch(function(e) {
+          console.log(e);
+          console.log("Server is down so.. KARMA INCREASE");
+          self.karma -= 1;
+          gossip.querySelector('.gossip-karma').textContent = self.karma;
+        });
+    });
+
+    gossip.querySelector('.positive-vote').addEventListener('mouseup', function() {
+      //No puedo usar this porq es un nuevo contexto verdad?
+      self.up()
+        .then(function(response) {
+          gossip.karma.textContent = response.data.ka_gossip;
+        })
+        .catch(function(e) {
+          console.log(e);
+          console.log("Server is down so.. KARMA INCREASE");
+          self.karma += 1;
+          gossip.querySelector('.gossip-karma').textContent = self.karma;
+        });
+    });
+
     return gossip;
   }
-
 }

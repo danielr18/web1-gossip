d4c057b21419848f4c664964fac64909bd1c068c
Daniel Reinoso <danielreinoso1807@gmail.com>
2016-12-07 22:11:44 -0400

    removed fake api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var ejs = require('ejs');
 var router = express.Router();
-let count = 0;
-var gossipArray = [];
 
 router.get('/', function(req, res) {
   res.redirect('/home');
@@ -30,76 +28,6 @@ router.get('/logs', function(req, res) {
 
 router.get('/login', function(req, res) {
   res.render('login');
-});
-
-router.get('/gossip/all', function(req,res){
-  console.log(gossipArray);
-  res.send({
-    message:'All gossips sent',
-    gossips: gossipArray
-  });
-});
-
-router.post('/gossip/up', function(req, res) {
-  console.log(req.body);
-  let id = req.body.id_gossip;
-  let index = gossipArray.map(function(gossip){
-    return gossip.id_gossip;
-  }).indexOf(id);
-  gossipArray[index].ka_gossip+=1;
-  res.send({message: 'Karma raised'});
-});
-
-router.post('/gossip/down', function(req, res) {
-  let id = req.body.id_gossip;
-  let index = gossipArray.map(function(gossip){
-    return gossip.id_gossip;
-  }).indexOf(id);
-  gossipArray[index].ka_gossip-=1;
-  res.send({message: 'Karma decreased'});
-});
-
-router.post('/gossip/create', function(req, res) {
-  count++;
-  let data = req.body;
-  let gossip = {
-    id_gossip : count,
-    id_usuario : data.id_usuario,
-    de_gossip : data.de_gossip,
-    ka_gossip : 0,
-    da_gossip : new Date(),
-    id_gossip_status: 1,
-    de_gossip_status: 'Avaliable'
-  };
-  gossipArray.push(gossip);
-
-  res.send({
-    message: 'Gossip added',
-  });
-});
-
-router.get('/gossip/delete',function(req, res) {
-  console.log(req.body);
-  let id = parseInt(req.query.id_gossip);
-  let index = gossipArray.map(function(gossip){
-    return gossip.id_gossip;
-  }).indexOf(id);
-  gossipArray[index].id_gossip_status = 0;
-  res.send({message: `Gossip ${id} deleted`});
-});
-
-router.post('/gossip/delete',function(req, res) {
-  console.log(req.body);
-  res.send("papo")
-});
-
-router.get('/admin/gossip/delete',function(req, res) {
-  let id = parseInt(req.query.id_gossip);
-  let index = gossipArray.map(function(gossip){
-    return gossip.id_gossip;
-  }).indexOf(id);
-  gossipArray[index].id_gossip_status = 0;
-  res.send({message: `Gossip ${id} deleted`});
 });
 
 
